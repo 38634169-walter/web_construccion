cf96e0c867a73d528f8330ba65e2f913069b6105
walter <walterdiaz9418@gmail.com>
2021-02-24 22:23:21 -0300

    cambios

--- js/menu-responsive.js
+++ js/menu-responsive.js
@@ -1,26 +1,31 @@
+
 $(document).ready(menu_responsive);
 
 
 function menu_responsive(){
+    
     $('.btn-hamburger').click(menu_aparecer);
     $('.cruz-icon').click(menu_desaparecer);
-    $('.btn-mn').click(menu_desaparecer); 
-}
-
-
-function menu_aparecer(){
+    $('.btn-mn').click(menu_desaparecer);
     
-    $('nav').animate({
-        left:'0'
-    });
-    $('.btn-hamburger').toggleClass('remove');
-    $('.cruz-icon').toggleClass('show'); 
+
+    function menu_aparecer(){
+        
+        $('nav').animate({
+            left:'0'
+        });
+        $('.btn-hamburger').toggleClass('remove');
+        $('.cruz-icon').toggleClass('show');         
+    }
+
+    function menu_desaparecer(){
+        
+        $('nav').animate({
+            left:'-100%'
+        });
+        $('.cruz-icon').toggleClass('show');
+        $('.btn-hamburger').toggleClass('remove');      
+    }
+    
 }
 
-function menu_desaparecer(){
-    $('nav').animate({
-        left:'-100%'
-    });
-    $('.cruz-icon').toggleClass('show');
-    $('.btn-hamburger').toggleClass('remove');
-}
