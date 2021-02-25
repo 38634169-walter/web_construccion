65f18e2355a79fa37f7a8e05c6ccbba15108f0c3
walter <walterdiaz9418@gmail.com>
2021-02-25 00:19:22 -0300

    cambios en javascript

--- js/menu-responsive.js
+++ js/menu-responsive.js
@@ -1,31 +1,30 @@
 
 $(document).ready(menu_responsive);
 
-
+let num=0;
 function menu_responsive(){
     
     $('.btn-hamburger').click(menu_aparecer);
     $('.cruz-icon').click(menu_desaparecer);
     $('.btn-mn').click(menu_desaparecer);
-    
-
-    function menu_aparecer(){
-        
-        $('nav').animate({
-            left:'0'
-        });
-        $('.btn-hamburger').toggleClass('remove');
-        $('.cruz-icon').toggleClass('show');         
-    }
-
-    function menu_desaparecer(){
-        
-        $('nav').animate({
-            left:'-100%'
-        });
-        $('.cruz-icon').toggleClass('show');
-        $('.btn-hamburger').toggleClass('remove');      
-    }
+    $('.top').click(menu_desaparecer);
+    $('section').click(menu_desaparecer);
+    $('footer').click(menu_desaparecer);
     
 }
 
+
+function menu_aparecer(){
+        
+    $('nav').css({"left":"0"});
+    $('.btn-hamburger').css({"display":"none"});
+    $('.cruz-icon').css({"display":"block"});   
+}    
+
+
+function menu_desaparecer(){
+    
+    $('nav').css({"left":"-100%"});
+    $('.btn-hamburger').css({"display":"block"});
+    $('.cruz-icon').css({"display":"none"});    
+}
